0bfb94f0641a0ad778474b8805a5342b38230e0a
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2024-11-27 18:16:18 -0500

    added two new medium problems solved

---
+++ src/Problems/LongestConsecutiveSequence.java
@@ -0,0 +1,28 @@
+package Problems;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+        //put the nums in a set for fast lookup
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int len = 0;
+        for (int num : nums) {
+            if (!numSet.contains(num - 1)) {
+                int localLength = 1;
+                int n = num;
+                while (numSet.contains(++n)){
+                    localLength++;
+                }
+                if (localLength > len) len = localLength;
+            }
+        }
+        return len;
+    }
+}
---
+++ src/Problems/ValidSudoku.java
@@ -0,0 +1,59 @@
+package Problems;
+
+import java.util.HashSet;
+
+public class ValidSudoku {
+
+    public boolean isValidSudoku(char[][] board) {
+        int size = 9;
+
+        HashSet<Character> columns = new HashSet<>();
+        HashSet<Character> rows = new HashSet<>();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; i++) {
+                char rowCell = board[i][j];
+                char columnCell = board[j][i];
+
+                if (rowCell != '.') {
+                    if (!rows.contains(rowCell)) {
+                        rows.add(rowCell);
+                    } else {
+                        return false;
+                    }
+                }
+
+                if (columnCell != '.') {
+                    if (!columns.contains(columnCell)) {
+                        columns.add(columnCell);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        int[][] offsets = {{1, 1}, {1, -1}, {1, 0}, {-1, 1}, {-1, 0}, {-1, -1}, {0, 1}, {0, -1}};
+        int[] cords = {1, 4, 7};
+
+        for(int i = 0; i < cords.length; i++) {
+            for (int j = 0; j < cords.length; j++) {
+                HashSet<Character> sections = new HashSet<>();
+                for (int[] offset : offsets) {
+                    char cell = board[offset[0]][offset[1]];
+                    if (cell != '.') {
+                        if (!sections.contains(cell)) {
+                            sections.add(cell);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+}
