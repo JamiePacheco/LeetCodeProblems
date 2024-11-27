83e982fba047433f05f2ab0418c4ac4725b98434
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2024-11-27 18:42:52 -0500

    ValidPars problem added

---
+++ src/Problems/ArraysHashing/EncodeDecode.java
@@ -0,0 +1,16 @@
+package Problems.ArraysHashing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EncodeDecode {
+
+    public String encode(List<String> strs) {
+        return strs.size() == 0 ? strs.stream().collect(Collectors.joining("#")) : "";
+    }
+
+    public List<String> decode(String str) {
+        return  List.of(str.isBlank() ? str.split("#") : null)
+    }
+}
--- src/Problems/Stack/ValidParentheses.java
+++ src/Problems/Stack/ValidParentheses.java
@@ -1,4 +1,26 @@
 package Problems.Stack;
 
+import java.util.Stack;
+
 public class ValidParentheses {
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> parStack = new Stack<>();
+            for (char par : s.toCharArray()) {
+                if (par == '(' || par == '[' || par == '{') {
+                    parStack.push(par);
+                }
+                if (par == ')') {
+                    if (parStack.isEmpty() || parStack.pop() != '(') return false;
+                }
+                if (par == ']') {
+                    if (parStack.isEmpty() || parStack.pop() != '[') return false;
+                }
+                if (par == '}') {
+                    if (parStack.isEmpty() || parStack.pop() != '{') return false;
+                }
+            }
+            return parStack.isEmpty();
+        }
+    }
 }
